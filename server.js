180ab5e486c5ee669ce518687f82aa608f76a6d2
Ruturaj <ruturaj2404@gmail.com>
2022-06-11 15:57:37 +0530

    context menu and folder animations

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const ObjectId = mongoose.Types.ObjectId;
 const path = require("path");
 const app = express();
@@ -13,7 +14,11 @@ db.once("open", () => {
 	console.log("Connected");
 });
 const filesys = {
-	"/User": { dirs: ["/Media", "/Docs", "/Code"], files: ["abc.pdf"], pwd: "/User" },
+	"/User": {
+		dirs: ["/Media", "/Docs", "/Code"],
+		files: ["abc.pdf"],
+		pwd: "/User",
+	},
 	"/User/Media": { files: "random.txt", pwd: "/User/Media" },
 	"/User/Docs": { files: "doc.pdf", pwd: "/User/Docs" },
 	"/User/Code": { files: "hello.cpp", pwd: "/User/Code" },
@@ -38,24 +43,28 @@ const folderSchema = new mongoose.Schema(
 	{ collection: "folders" }
 );
 const folderModel = mongoose.model("Folder", folderSchema, "folders");
-// folderModel.find({ "parent.parentId._id": ObjectId("6295a9963526db756e309672") }).then((docs) => {
-// 	console.log(docs);
-// });
+
 app.use("/static", express.static(path.resolve("./staticfiles")));
 app.use(express.json());
+app.use(cors());
+
 app.get("/", (req, res) => {
 	res.render(path.resolve("./public/fstest.ejs"), { path: filesys["/User"] });
 });
 app.post("/createfolder", async (req, res) => {
-	const { parent, pwd } = req.body;
-	console.log(parent, pwd);
-	const parentId = await folderModel.findOne({ pwd: parent }).select("_id");
+	const { pwd, parid } = req.body;
+	// console.log(parent, pwd);
+	const parentId = await folderModel.findById(parid).select("_id pwd");
 	const parentObj = {
-		parentName: parent,
-		parentId: parentId,
+		parentName: parentId.pwd,
+		parentId: parentId._id,
 	};
 	// res.json(parentObj);
-	const folder = await folderModel.findOne({ "parent.parentName": parent, pwd: pwd });
+	const folder = await folderModel.findOne({
+		"parent.parentId": ObjectId(parid),
+		pwd: pwd,
+	});
+	// res.json({ folders: folder, id: parentId });
 	if (folder == null) {
 		folderModel.create({ parent: parentObj, pwd: pwd, folders: [], files: [] });
 		res.json({ msg: "Created a new folder" });
@@ -66,9 +75,19 @@ app.post("/createfolder", async (req, res) => {
 app.get("/folders/:folder", async (req, res) => {
 	const { folder } = req.params;
 	if (mongoose.isObjectIdOrHexString(folder) && folder != rootID) {
-		const folds = await folderModel.find({ $or: [{ _id: folder }, { "parent.parentId._id": ObjectId(folder) }] }).select("_id pwd parent.parentId._id");
-		// res.json({ parentname:folds[0].pwd,parentid: folds[0].parent.parentId._id, folders: folds.slice(1) });
-		res.render(path.resolve("./public/folderview.ejs"), { parentid: folds[0].parent.parentId._id, folders: folds.slice(1), parname: folds[0].pwd });
+		const folds = await folderModel
+			.find({
+				$or: [
+					{ _id: ObjectId(folder) },
+					{ "parent.parentId": ObjectId(folder) },
+				],
+			})
+			.select("_id pwd parent");
+		res.render(path.resolve("./public/folderview.ejs"), {
+			parentid: folds[0].parent.parentId,
+			folders: folds.slice(1),
+			parname: folds[0].pwd,
+		});
 	} else {
 		res.status(404).sendFile(path.resolve("./public/404.html"));
 	}
